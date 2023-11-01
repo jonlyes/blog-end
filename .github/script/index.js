c7fc457f385190a57827aeb821979314b757acba
jonlyes <3236526598@qq.com>
2023-11-01 17:25:38 +0800

    自动化部署

---
+++ .github/script/index.js
@@ -0,0 +1,28 @@
+const fs = require('fs')
+// 配置.env
+
+const env = process.env | {}
+
+const content = `
+# 服务器
+APP_HOST=${env.APP_HOST}
+APP_PROT=${env.APP_PROT}
+
+# 数据库
+MYSQL_HOST=${env.MYSQL_HOST}
+MYSQL_PORT=${env.MYSQL_PROT}
+MYSQL_USER=${env.MYSQL_USER}
+MYSQL_PASSWORD=${env.MYSQL_PASSWORD}
+MYSQL_NAME=${env.MYSQL_NAME}
+
+# token key
+JWT_KEY=jonlyesblog
+`
+
+fs.writeFile('./.env', content, err => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('.env配置成功');
+    }
+})
